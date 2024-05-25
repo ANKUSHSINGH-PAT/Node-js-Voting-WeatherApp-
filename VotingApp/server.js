e4f9055233c89b913cfddf03df47d1b62468116a
Ankush <singhankush06@gmail.com>
2024-05-25 12:32:50 +0530

    All files committed

---
+++ VotingApp/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express();
+const db = require('./db');
+require('dotenv').config();
+
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
+
+// Import the router files
+const userRoutes = require('./routes/userRoutes');
+const candidateRoutes = require('./routes/candidateRoutes');
+
+const logrequest=(req,res,next=>{
+    console.log(`${new Date().toLocaleString}`,'request made at'`${req.originalUrl}`);
+    next();
+})
+
+app.use(logrequest);
+
+// Use the routers
+app.use('/user', userRoutes);
+app.use('/candidate', candidateRoutes);
+
+
+app.listen(PORT, ()=>{
+    console.log('listening on port 3000');
+})
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello")
+console.info("Danger")
+//console.log({Age: 23,Name:"Ankush"})
+console.table({Name:"Ankush"})
